59ebdf76eca1e551f07703f6b61205d759d7a8f1
daole <lechanhdao240688@gmail.com>
2015-10-18 11:51:09 +0700

    initial commit

---
+++ app/src/main/java/com/dreamdigitizers/drugmanagement/MyApplication.java
@@ -0,0 +1,9 @@
+package com.dreamdigitizers.drugmanagement;
+
+import android.app.Application;
+
+/**
+ * Created by BichThao on 18/10/2015.
+ */
+public class MyApplication extends Application {
+}
